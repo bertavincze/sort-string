400bb8850f802c45d621d38fe8c146d86202f862
Berta Vincze <berta.vincze@gmail.com>
2019-06-17 13:21:55 +0200

    Add solution

--- Program.cs
+++ Program.cs
@@ -10,6 +10,12 @@ namespace SortString
     {
         static void Main(string[] args)
         {
+            string str = "Microsoft .NET Framework 2.0 Application Development Foundation";
+            string[] words = str.Split(' ');
+            // Array.Sort(words);
+            string s = string.Join(" ", words);
+            Console.WriteLine(s);
+            Console.ReadKey();
         }
     }
 }
